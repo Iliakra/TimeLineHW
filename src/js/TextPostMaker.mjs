d41a61d2b5287850416d92f5b6501ded272aba5e
Iliakra <kravus27@gmail.com>
2021-07-20 13:14:51 +0300

    new commit

--- src/js/TextPostMaker.mjs
+++ src/js/TextPostMaker.mjs
@@ -3,10 +3,42 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable indent */
 /* eslint-disable linebreak-style */
+import formatDate from './formatDate.mjs';
+
 export default class TextPostMaker {
-    constructor() {
+    constructor(value) {
+        this.value = value;
         this.container = null;
-        this.closeButton = null;
+        this.positionErrorElement = null;
+        this.longitude = null;
+        this.latitude = null;
+    }
+
+    build() {
+        const textPostContainer = document.createElement('div');
+        textPostContainer.classList.add('text-post-container');
+
+        const postTextContent = document.createElement('p');
+        postTextContent.classList.add('post-text-content');
+        postTextContent.textContent = this.value;
+
+        const coordsContent = document.createElement('p');
+        coordsContent.classList.add('coords-content');
+        coordsContent.textContent = `${this.longitude}, ${this.latitude}`;
+
+        const dateContent = document.createElement('p');
+        dateContent.classList.add('date-content');
+        const date = new Date();
+        const dateString = formatDate(date);
+        dateContent.textContent = dateString;
+
+        textPostContainer.appendChild(postTextContent);
+        textPostContainer.appendChild(coordsContent);
+        textPostContainer.appendChild(dateContent);
+
+        this.container.appendChild(textPostContainer);
+
+        this.container.scrollTo(0, this.container.scrollHeight);
     }
 
     geoLocation() {
@@ -14,10 +46,79 @@ export default class TextPostMaker {
             navigator.geolocation.getCurrentPosition(
                 position => {
                     const { latitude, longitude } = position.coords;
-                    console.log('lati', latitude);
-                }, (positionError) => console.log('ERROR'),
+                    this.longitude = longitude;
+                    this.latitude = latitude;
+                    this.build();
+                }, (positionError) => this.customGeoLocation(),
             );
         }
+    }
+
+    customGeoLocation() {
+        const positionErrorContainer = document.createElement('div');
+        positionErrorContainer.classList.add('position-error-container');
+        this.positionErrorElement = positionErrorContainer;
+
+        const positionErrorTitle = document.createElement('p');
+        positionErrorTitle.textContent = 'Что-то пошло не так';
+        positionErrorContainer.appendChild(positionErrorTitle);
+
+        const positionErrorTextContent = document.createElement('p');
+        positionErrorTextContent.textContent = 
+            'К сожалению, нам не удалось определить ваше местоположение, пожалуйста дайте разрешение на использование геолокации, либо введите координаты вручную';
+
+        positionErrorContainer.appendChild(positionErrorTextContent);
+
+        const customCoordsInputTitle = document.createElement('p');
+        customCoordsInputTitle.textContent = 'Широта и долгота через запятую';
+        positionErrorContainer.appendChild(customCoordsInputTitle);
+
+        const customCoordsInput = document.createElement('input');
+        customCoordsInput.classList.add('custom-coords-input');
+        positionErrorContainer.appendChild(customCoordsInput);
+
+        const buttonsContainer = document.createElement('div');
+        buttonsContainer.classList.add('buttons-container');
+
+        const cancelButton = document.createElement('button');
+        cancelButton.classList.add('cancel-button');
+        cancelButton.textContent = 'Cancel';
+        cancelButton.addEventListener('click', this.closeHandler.bind(this));
+        buttonsContainer.appendChild(cancelButton);
+
+        const okButton = document.createElement('button');
+        okButton.classList.add('ok-button');
+        okButton.textContent = 'OK';
+        okButton.addEventListener('click', this.addingCustomCoords.bind(this));
+        buttonsContainer.appendChild(okButton);
+
+        positionErrorContainer.appendChild(buttonsContainer);
+        this.container.appendChild(positionErrorContainer);
+    }
+
+    closeHandler() {
+        this.positionErrorElement.remove();
+    }
+
+    addingCustomCoords() {
+        const customCoordsInput = document.getElementsByClassName('custom-coords-input')[0];
+        const customCoordsValue = customCoordsInput.value;
+        const validity = this.customCoordsValidity(customCoordsValue);
+        if (validity) {
+            //this.value = customCoordsValue;
+            this.build();
+            this.closeHandler();
+        } else {
+            alert('Неверно введены координаты!');
+        }
+    }
+
+    customCoordsValidity(value) {
+        const result = value.match(/^\[?\d+\.\d+,\s?\d+\.\d+\]?$/);
+        if (result === null) {
+            return false;
+        } 
+            return true;
     }
 
     bindToDom(container) {
--- src/js/app.mjs
+++ src/js/app.mjs
@@ -1,14 +1,15 @@
 /* eslint-disable linebreak-style */
 import TextPostMaker from './TextPostMaker.mjs';
 // eslint-disable-next-line linebreak-style
-
-const newTextPost = new TextPostMaker();
+const input = document.getElementsByClassName('timeline-input')[0];
 
 const submitHandler = (event) => {
     event.preventDefault();
-    newTextPost.bindToDom(document.querySelector('timeline-container'));
+    let inputValue = input.value;
+    const newTextPost = new TextPostMaker(inputValue);
+    newTextPost.bindToDom(document.getElementsByClassName('timeline-posts-container')[0]);
     newTextPost.geoLocation();
 };
 
-const input = document.getElementById('timeline-input-form');
-input.addEventListener('submit', submitHandler);
+const form = document.getElementById('timeline-input-form');
+form.addEventListener('submit', submitHandler);
